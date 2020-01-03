2b9f8e887861b5a573bee10577ea7e798f94ce7b
afcso <jonathan.nessier@vtg.admin.ch>
2020-01-03 10:02:13 +0100

    Updated deps and build pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,50 @@
-var gulp = require('gulp'),
-        fs = require('fs'),
-        util = require('gulp-util'),
-        rename = require('gulp-rename'),
-        uglify = require('gulp-uglify-es').default,
-        injectString = require('gulp-inject-string'),
-        runSequence = require('run-sequence');
+let gulp = require('gulp'),
+    fs = require('fs'),
+    terser = require('gulp-terser'),
+    rename = require('gulp-rename'),
+    injectString = require('gulp-inject-string'),
+    concat = require('gulp-concat'),
+    replace = require('gulp-replace');
 
-var pjson = require('./package.json');
+let package = require('./package.json');
 
-var sourceHeader = fs
-        .readFileSync('./src/source-header.txt', 'utf8')
-        .replace('{VERSION}', pjson.version)
-        .replace('{YEAR}', new Date().getFullYear());
+let sourceHeader = fs
+    .readFileSync(package.buildOpts.common.sourceHeader, 'utf8')
+    .replace('{VERSION}', package.version)
+    .replace('{YEAR}', (new Date()).getFullYear().toString())
+    .replace('{AUTHOR}', package.author);
 
-gulp.task('js:build', function (cb) {
-    return gulp.src(['./src/bootstrap-fileselect.js'])
+gulp.task('js:build', function () {
+    let result = gulp.src(package.buildOpts.jsBuild.src);
+
+    if (Array.isArray(package.buildOpts.jsBuild.src)) {
+        result = result.pipe(concat(package.buildOpts.jsBuild.concatName));
+    }
+
+    return result
             .pipe(injectString.prepend(sourceHeader + '\n'))
-            .pipe(gulp.dest('./dist'));
+            .pipe(gulp.dest(package.buildOpts.jsBuild.dest));
 });
 
-gulp.task('js:minify', function (cb) {
-    return gulp.src(['./src/bootstrap-fileselect.js'])
-            .pipe(uglify().on('error', util.log))
-            .pipe(rename({suffix: '.min'}))
-            .pipe(injectString.prepend(sourceHeader + '\n'))
-            .pipe(gulp.dest('./dist'));
+gulp.task('js:minify', function () {
+    return gulp.src(package.buildOpts.jsMinify.src)
+        .pipe(terser({
+            output: {
+                comments: false
+            }
+        }))
+        .pipe(rename({
+            suffix: package.buildOpts.jsMinify.renameSuffix
+        }))
+        .pipe(replace(/\r?\n|\r/g, ''))
+        .pipe(injectString.prepend(sourceHeader + '\n'))
+        .pipe(gulp.dest(package.buildOpts.jsMinify.dest));
 });
 
-gulp.task('watch', function () {
-    gulp.watch(['./src/bootstrap-fileselect.js'], function () {
-        runSequence('js:build', 'js:minify');
-    });
+gulp.task('js:rebuild', gulp.series('js:build', 'js:minify'));
+gulp.task('src:rebuild', gulp.series('js:rebuild'));
+
+gulp.task('src:watch', function () {
+    gulp.watch(package.buildOpts.srcWatch.path, gulp.series('js:rebuild'));
 });
+
